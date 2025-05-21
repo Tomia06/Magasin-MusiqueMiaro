a545b39fa395575839585c436aac10216e688b65
Tomia06 <miaroratsimba@gmail.com>
2025-05-21 17:19:31 +0200

    Magasin Musique

---
+++ app.js
@@ -0,0 +1,17 @@
+fetch("musique.json")
+    .then(musique => musique.json())
+    .then(magasin => {
+        afficher(magasin);
+    });
+
+
+// ROle: affiche les instruments dans la div qui a l'id di&vProduit
+// Parametre : tableau, le tableau des instruments
+// retour: rien 
+function afficher(tableau) {
+    tableau.produits.forEach(articles => {
+            document.getElementById("divProduit").innerHTML += `<img src=${articles.image} alt="" class="w100">
+                <h2>${articles.nom}</h2>
+                <p>${articles.description}</p>`
+        });
+}                                                   
